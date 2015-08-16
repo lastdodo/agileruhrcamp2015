c4b7d0f0ad4e6a229691647bdc624966938f1ea2
Dominik Rose <dominik@Dominiks-MacBook-Pro.local>
2015-08-16 21:17:27 +0200

    Initial kirby

---
+++ camp/2016/site/snippets/footer.php
@@ -0,0 +1,35 @@
+	</main>
+<footer id="footer">
+	<div class="center">
+		<div class="top">
+			<h2><span>WIR FREUEN UNS AUF EUCH</span></h2>
+		</div>
+		<div class="bottom">
+			<p class="copyright">© BarCampRuhr 2016<br>Images CC Simon Bierwald</p>
+			<!-- SOCIAL ICONS -->
+			<ul class="social-icons list-inline">
+				<li class="twitter"><a href="https://twitter.com/BarcampRuhr">
+					<img src="<?php url('assets/images/icons/twitter.png')?>" alt=""></a>
+				</li>
+			</ul>
+		</div>
+	</div>
+</footer>
+	<!-- /#footer -->
+<!-- Piwik -->
+<script type="text/javascript">
+  var _paq = _paq || [];
+  _paq.push(['trackPageView']);
+  _paq.push(['enableLinkTracking']);
+  (function() {
+    var u="//mcwiwa-services.de/stats/";
+    _paq.push(['setTrackerUrl', u+'piwik.php']);
+    _paq.push(['setSiteId', 12]);
+    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+    g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
+  })();
+</script>
+<noscript><p><img src="//mcwiwa-services.de/stats/piwik.php?idsite=12" style="border:0;" alt="" /></p></noscript>
+<!-- End Piwik Code -->
+</body>
+</html>
